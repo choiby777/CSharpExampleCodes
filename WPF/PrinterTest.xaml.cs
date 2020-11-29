0389a947e78c09dd960aed5e51eab803d5825482
choiby777 <choiby777@gmail.com>
2020-11-29 15:23:46 +0900

    add Print functions

--- WPF/PrinterTest.xaml.cs
+++ WPF/PrinterTest.xaml.cs
@@ -26,13 +26,86 @@ namespace Examples
 
 			btnSelectFile.Click += BtnSelectFile_Click;
 			btnPrint.Click += BtnPrint_Click;
+			btnPrintEx.Click += BtnPrintEx_Click;
+			btnPrintImageBox.Click += BtnPrintImageBox_Click;
+		}
+
+		private void PrintVisual(Visual v)
+		{
+			System.Windows.FrameworkElement e = v as System.Windows.FrameworkElement;
+			if (e == null)
+				return;
+
+			PrintDialog pd = new PrintDialog();
+			if (pd.ShowDialog() == true)
+			{
+				//store original scale
+				Transform originalScale = e.LayoutTransform;
+				//get selected printer capabilities
+				System.Printing.PrintCapabilities capabilities = pd.PrintQueue.GetPrintCapabilities(pd.PrintTicket);
+
+				//get scale of the print wrt to screen of WPF visual
+				double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / e.ActualWidth, capabilities.PageImageableArea.ExtentHeight /
+							   e.ActualHeight);
+
+				//Transform the Visual to scale
+				e.LayoutTransform = new ScaleTransform(scale, scale);
+
+				//get the size of the printer page
+				System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+
+				//update the layout of the visual to the printer page size.
+				e.Measure(sz);
+				e.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
+
+				//now print the visual to printer to fit on the one page.
+				pd.PrintVisual(v, "My Print");
+
+				//apply the original transform.
+				e.LayoutTransform = originalScale;
+			}
+		}
+
+		private void BtnPrintImageBox_Click(object sender, RoutedEventArgs e)
+		{
+			Visual v = imgPrintImage as Visual;
+
+			PrintVisual(v);
+		}
+
+
+		private void BtnPrintEx_Click(object sender, RoutedEventArgs e)
+		{
+			PrintDialog dialog = new PrintDialog();
+			if (dialog.ShowDialog() != true) return;
+
+			StackPanel myPanel = new StackPanel();
+			myPanel.Margin = new Thickness(10);
+
+			Image myImage = new Image();
+			myImage.Width = 128;
+			myImage.Stretch = Stretch.Uniform;
+			myImage.Source = new BitmapImage(new Uri(txtImageFilePath.Text, UriKind.Absolute));
+
+			myPanel.Children.Add(myImage);
+
+			//TextBlock myBlock = new TextBlock();
+			//myBlock.Text = "A Great Image.";
+			//myBlock.TextAlignment = TextAlignment.Center;
+
+			//myPanel.Children.Add(myBlock);
+
+			myPanel.Measure(new Size(dialog.PrintableAreaWidth, dialog.PrintableAreaHeight));
+			myPanel.Arrange(new Rect(new Point(0, 0), myPanel.DesiredSize));
+
+			dialog.PrintVisual(myPanel, "A Great Image.");
 		}
 
 		private void BtnPrint_Click(object sender, RoutedEventArgs e)
 		{
-			PrintDialog pringDlg = new PrintDialog();
+			PrintDialog printDlg = new PrintDialog();
 
-			if (pringDlg.ShowDialog() == true)
+			if (printDlg.ShowDialog() == true)
 			{
 				ImageSource printImage = GetImageSource(txtImageFilePath.Text);
 
@@ -41,7 +114,7 @@ namespace Examples
 				dc.DrawImage(printImage, new Rect { Width = printImage.Width, Height = printImage.Height });
 				dc.Close();
 
-				pringDlg.PrintVisual(visual , "Print Test");
+				printDlg.PrintVisual(visual, "Print Test");
 			}
 		}
 
